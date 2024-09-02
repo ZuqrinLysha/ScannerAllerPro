cb2b044d44fb7ea1e7d0b51692e4e3ec78689b28
ZuqrinLysha <eshazuq@gmail.com>
2024-09-02 15:18:51 +0800

    tinggal forgot password je tk boleh send email lagi

---
+++ app/src/main/java/com/example/scannerallerpro/ForgotPassword.java
@@ -0,0 +1,81 @@
+package com.example.scannerallerpro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+    Button btnReset, btnCancel;
+    EditText txtForgotPassword;
+    ProgressBar progressBarForgotPassword;
+    FirebaseAuth Auth;
+    String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        btnReset = findViewById(R.id.btnReset);
+        btnCancel = findViewById(R.id.btnCancel);
+        txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        progressBarForgotPassword = findViewById(R.id.progressBarForgotPassword);
+        Auth = FirebaseAuth.getInstance();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = txtForgotPassword.getText().toString().trim();
+                if (!TextUtils.isEmpty(email)) {
+                    ResetPassword();
+                } else {
+                    txtForgotPassword.setError("Email cannot be empty");
+                    txtForgotPassword.requestFocus();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void ResetPassword() {
+        progressBarForgotPassword.setVisibility(View.VISIBLE);
+        btnReset.setVisibility(View.INVISIBLE);
+
+        Auth.sendPasswordResetEmail(email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(ForgotPassword.this, "Check Your Email. Password reset link has been sent.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ForgotPassword.this, LogIn.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ForgotPassword.this, "Error! Reset Password Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBarForgotPassword.setVisibility(View.INVISIBLE);
+                        btnReset.setVisibility(View.VISIBLE);
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/scannerallerpro/LogIn.java
+++ app/src/main/java/com/example/scannerallerpro/LogIn.java
@@ -1,28 +1,36 @@
 package com.example.scannerallerpro;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Objects;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
-    //Declare views
+    // Declare views
     EditText txtEmailL, txtPassword;
     TextView txtSignUp, txtForgotPassword;
     Button btnLogin;
+
+    // Declare Firebase Auth instance
+    FirebaseAuth auth;
+
+    // Counter for login attempts
+    private int loginAttempts = 0;
+    private static final int MAX_ATTEMPTS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +42,20 @@ public class LogIn extends AppCompatActivity {
         txtPassword = findViewById(R.id.txtPassword);
         btnLogin = findViewById(R.id.btnLogin);
         txtSignUp = findViewById(R.id.txtSignUp);
+        txtForgotPassword = findViewById(R.id.txtForgotPassword);
+
+        // Initialize Firebase Auth instance
+        auth = FirebaseAuth.getInstance();
 
         // Set listener for the Log In button
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!validateEmail() | !validatePassword()) {
+                if (!validateEmail() || !validatePassword()) {
                     // Validation failed, do nothing
                     return;
                 } else {
-                    checkUser();
+                    loginUser();
                 }
             }
         });
@@ -56,83 +68,70 @@ public class LogIn extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, ForgotPassword.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 
-    //Validate the email input field
+    // Validate the email input field
     public Boolean validateEmail() {
         String val = txtEmailL.getText().toString();
         if (val.isEmpty()) {
-            //Show an error if the email field is empty
             txtEmailL.setError("Email cannot be empty");
             return false;
         } else {
-            // Clear any error if the email is invalid
             txtEmailL.setError(null);
             return true;
         }
     }
 
-    //Validate the password input field
+    // Validate the password input field
     public Boolean validatePassword() {
         String val = txtPassword.getText().toString();
         if (val.isEmpty()) {
-            //Show an error if the password field is empty
             txtPassword.setError("Password cannot be empty");
             return false;
         } else {
-            // Clear any error if the password is valid
             txtPassword.setError(null);
             return true;
         }
     }
 
-    //Check if the user exists in the database
-    public void checkUser() {
-        // Get user input such as email and password
+    // Log in the user using Firebase Authentication
+    public void loginUser() {
         String userEmail = txtEmailL.getText().toString().trim();
         String userPassword = txtPassword.getText().toString().trim();
 
-        // Firebase Database instance
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
-        Query checkUserDatabase = reference.orderByChild("email").equalTo(userEmail);
-
-        // Check if the user exists in the database
-        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+        auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            // If the user exists, check the password
-            public void onDataChange(DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
-                        // Get the password from the database
-                        String passwordFromDB = userSnapshot.child("password").getValue(String.class);
-
-                        // Check if the password matches the user's input
-                        if (Objects.equals(passwordFromDB, userPassword)) {
-                            Intent intent = new Intent(LogIn.this, HomePage.class); // Navigate to HomePage
-                            startActivity(intent);
-                            finish();
-                            // Exit the method after successful login
-                        } else {
-                            // Show an error if the password is invalid
-                            txtPassword.setError("Invalid Credentials");
-                            txtPassword.requestFocus();
-                        }
-                        // Exit the loop and method after checking credentials
-                        return;
-                    }
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Successful login
+                    loginAttempts = 0;
+                    Intent intent = new Intent(LogIn.this, HomePage.class); // Navigate to HomePage
+                    startActivity(intent);
+                    finish(); // Optional: finish the LogIn activity so the user cannot navigate back to it
                 } else {
-                    // Show an error if the user does not exist
-                    txtEmailL.setError("User does not exist");
-                    txtEmailL.requestFocus();
-                }
-            }
+                    loginAttempts++;
+                    txtPassword.setError("Invalid Credentials");
+                    txtPassword.requestFocus();
 
-            @Override
-            // Handle error
-            public void onCancelled(DatabaseError error) {
+                    if (loginAttempts > MAX_ATTEMPTS) {
+                        // Navigate to ForgotPasswordActivity
+                        Intent intent = new Intent(LogIn.this, ForgotPassword.class);
+                        startActivity(intent);
+                        finish(); // Optional: finish the LogIn activity so the user cannot navigate back to it
+                    }
+                }
             }
         });
     }
-
 }
-
