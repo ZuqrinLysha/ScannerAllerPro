d27404e723ec439d535d69034066f131b718d9db
ZuqrinLysha <eshazuq@gmail.com>
2024-10-08 06:44:01 +0800

    betulkan dekat time poster

--- app/src/main/java/com/example/scannerallerpro/HomeFragment.java
+++ app/src/main/java/com/example/scannerallerpro/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.scannerallerpro;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +12,15 @@ import androidx.viewpager2.widget.ViewPager2;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.scannerallerpro.R;
-
 public class HomeFragment extends Fragment {
 
     private ViewPager2 viewPager;
-    private int[] images = {R.drawable.poster1, R.drawable.poster2, R.drawable.poster3
-    ,R.drawable.poster4, R.drawable.poster5, R.drawable.poster6, R.drawable.poster7}; // Your image resources
+    private int[] images = {R.drawable.poster1, R.drawable.poster2, R.drawable.poster3,
+            R.drawable.poster4, R.drawable.poster5, R.drawable.poster6, R.drawable.poster7}; // Your image resources
+    private Handler handler;
+    private Runnable runnable;
     private int currentPage = 0;
+    private final int delay = 3000; // Delay in milliseconds for auto sliding (3 seconds)
 
     @Nullable
     @Override
@@ -30,39 +32,40 @@ public class HomeFragment extends Fragment {
         viewPager.setAdapter(adapter);
 
         // Set up click listeners for each CardView
-        CardView cardAllergicHistory = view.findViewById(R.id.cardAllergicHistory);
-        cardAllergicHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(new AllergicHistoryFragment());
-            }
-        });
+        setupCardViewListeners(view);
 
-        CardView cardEmergencyContact = view.findViewById(R.id.cardEmergencyContact);
-        cardEmergencyContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(new ViewContactFragment());
-            }
-        });
-
-        CardView cardScanner = view.findViewById(R.id.cardScanner);
-        cardScanner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(new ScannerFragment());
-            }
-        });
-
-        CardView cardKnowledge = view.findViewById(R.id.cardKnowledge);
-        cardKnowledge.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(new KnowledgeFragment());
-            }
-        });
+        // Set up auto sliding
+        setupAutoSliding();
 
         return view;
+    }
+
+    private void setupCardViewListeners(View view) {
+        CardView cardAllergicHistory = view.findViewById(R.id.cardAllergicHistory);
+        cardAllergicHistory.setOnClickListener(v -> openFragment(new AllergicHistoryFragment()));
+
+        CardView cardEmergencyContact = view.findViewById(R.id.cardEmergencyContact);
+        cardEmergencyContact.setOnClickListener(v -> openFragment(new ViewContactFragment()));
+
+        CardView cardScanner = view.findViewById(R.id.cardScanner);
+        cardScanner.setOnClickListener(v -> openFragment(new ScannerFragment()));
+
+        CardView cardKnowledge = view.findViewById(R.id.cardKnowledge);
+        cardKnowledge.setOnClickListener(v -> openFragment(new KnowledgeFragment()));
+    }
+
+    private void setupAutoSliding() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == images.length) {
+                    currentPage = 0; // Reset to the first image
+                }
+                viewPager.setCurrentItem(currentPage++, true); // Slide to the next image
+                handler.postDelayed(this, delay); // Repeat this runnable code block again after 'delay' milliseconds
+            }
+        };
     }
 
     private void openFragment(Fragment fragment) {
@@ -70,5 +73,17 @@ public class HomeFragment extends Fragment {
         transaction.replace(R.id.fragment_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, delay); // Start auto sliding
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable); // Stop auto sliding
     }
 }
